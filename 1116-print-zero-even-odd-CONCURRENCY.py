b5492562e0d339dc129eb94509e5e8b5ddaae9a4
RichStone <richard.had@hotmail.de>
2020-01-11 06:05:32 +0100

    crazy conditions attempt

--- 1116-print-zero-even-odd-CONCURRENCY.py
+++ 1116-print-zero-even-odd-CONCURRENCY.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Condition, enumerate
 
 
 def printNumber(num):
@@ -8,22 +8,70 @@ def printNumber(num):
 class ZeroEvenOdd:
     def __init__(self, n):
         self.n = n
+        self.count = 0
+        self.zeros_turn = True
+        self.condition = Condition()
+
+    def is_zeros_turn(self):
+        return self.zeros_turn
+
+    def is_even_turn(self):
+        return not self.zeros_turn and self.count % 2 == 0
+
+    def is_odd_turn(self):
+        return not self.zeros_turn and self.count % 2 != 0
+
 
     # printNumber(x) outputs "x", where x is an integer.
     def zero(self, printNumber: 'Callable[[int], None]') -> None:
-        printNumber(0)
+        while True:
+            try:
+                if self.count < self.n:
+                    raise Exception('terminate B')
+            except:
+                print('no more evens needed')
+            with self.condition:
+                self.condition.wait_for(self.is_zeros_turn)
+                printNumber(0)
+                if self.count == 0:
+                    self.count += 1
+                self.zeros_turn = False
+                self.condition.notify_all()
+                print(enumerate())
 
     def even(self, printNumber: 'Callable[[int], None]') -> None:
-        printNumber(2)
+        while True:
+            try:
+                if self.n - self.count < 2:
+                    raise Exception('terminate B')
+            except:
+                print('no more evens needed')
+            with self.condition:
+                self.condition.wait_for(self.is_even_turn)
+                printNumber(self.count)
+                self.count += 1
+                self.zeros_turn = True
+                self.condition.notify_all()
+                print(enumerate())
+
 
     def odd(self, printNumber: 'Callable[[int], None]') -> None:
-        printNumber(1)
+        while True:
+
+            with self.condition:
+                self.condition.wait_for(self.is_odd_turn)
+                printNumber(self.count)
+                self.count += 1
+                self.zeros_turn = True
+                self.condition.notify_all()
+                print(enumerate())
 
 
 if __name__ == '__main__':
     instance = ZeroEvenOdd(2)
-    A = Thread(target=instance.zero, args=(printNumber,))
-    B = Thread(target=instance.even, args=(printNumber,))
-    C = Thread(target=instance.odd, args=(printNumber,))
-    A.run()
-    C.run()
+    A = Thread(target=instance.zero, args=(printNumber,), name='A')
+    B = Thread(target=instance.even, args=(printNumber,), name='B')
+    C = Thread(target=instance.odd, args=(printNumber,), name='C')
+    A.start()
+    B.start()
+    C.start()
--- boiler.py
+++ boiler.py
@@ -22,3 +22,35 @@ if __name__ == '__main__':
         print('output: ' + output)
         print('#############')
 
+# CONCURRENCY BOILER
+
+from threading import Thread, Condition
+
+
+def printNumber(num):
+    print(num, end='')
+
+
+class ZeroEvenOdd:
+    def __init__(self, n):
+        self.n = n
+
+    # printNumber(x) outputs "x", where x is an integer.
+    def zero(self, printNumber: 'Callable[[int], None]') -> None:
+        printNumber(0)
+
+    def even(self, printNumber: 'Callable[[int], None]') -> None:
+
+        printNumber(2)
+
+    def odd(self, printNumber: 'Callable[[int], None]') -> None:
+        printNumber(1)
+
+
+if __name__ == '__main__':
+    instance = ZeroEvenOdd(3)
+    A = Thread(target=instance.zero, args=(printNumber,))
+    B = Thread(target=instance.even, args=(printNumber,))
+    C = Thread(target=instance.odd, args=(printNumber,))
+    A.run()
+    C.run()
